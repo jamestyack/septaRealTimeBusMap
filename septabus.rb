428be35ff546e255468f265dd33a68fd42a1070a
James Tyack <jamestyack@gmail.com>
2013-06-03 00:04:18 -0400

    cleaned up unused path in Sinatra

--- septabus.rb
+++ septabus.rb
@@ -12,9 +12,3 @@ get '/septa/bus/:id' do
 	RestClient.get "http://www3.septa.org/transitview/bus_route_data/#{params[:id]}"
 end
 
-get '/septa/buses' do
-	
-	buses = { "route" => "12", "lat" => "39.944649", "long" => "-75.180679" }
-	content_type "application/json"
-	"#{buses.to_json}"
-end
